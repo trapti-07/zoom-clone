8cc37d3a99f2da7b6339374bc4e5ecd6207b6fc2
trapti katre <traptikatre2006@gmail.com>
2026-06-03 00:55:07 +0530

    Fix import paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
 import { AuthProvider } from './contexts/AuthContext';
 import VideoMeetComponent from './pages/VideoMeet';
-import HomeComponent from './pages/home';
+import HomeComponent from './pages/Home';
 import History from './pages/history';
 
 function App() {
