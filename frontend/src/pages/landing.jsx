776669ef208c8fa61279933602e76f5aa09c5022
trapti katre <traptikatre2006@gmail.com>
2026-06-03 11:02:25 +0530

    Changes

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../App.css"
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
 export default function LandingPage() {
 
 
